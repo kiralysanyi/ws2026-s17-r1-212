87949662f3d58e808599b6fa9840129f2dc02671
Király Sándor <kiralysandor1986@gmail.com>
2025-02-15 19:52:57 +0100

    - Added navigation to header

--- phase-2-src/src/components/Header.jsx
+++ phase-2-src/src/components/Header.jsx
@@ -2,17 +2,17 @@ import { Fragment } from "react"
 import CheckSvg from "../assets/check.svg"
 import MaximizeSvg from "../assets/maximize.svg"
 
-function Header({ currentStep, completedSteps, onMaximizeClick }) {
+function Header({ currentStep, completedSteps, onMaximizeClick, onNavigate }) {
     const steps = [1, 2, 3, 4]
 
     return (
-        <>
+        <Fragment>
             <header className="header">
                 <h1>Register a new location</h1>
                 <div className="steps">
                     {steps.map((num) => (
                         <Fragment key={num}>
-                            <button className={`step ${currentStep === num ? 'current' : ''} ${completedSteps >= num ? 'done' : ''}`}>{num}</button>
+                            <button onClick={() => { onNavigate(num) }} className={`step ${currentStep === num ? 'current' : ''} ${completedSteps >= num ? 'done' : ''}`} disabled={completedSteps + 1 < num}>{num}</button>
                             {num != steps.length ? <div className={`step-divider ${completedSteps >= num ? '' : 'dashed'}`}></div> : ""}
                         </Fragment>
                     ))}
@@ -23,7 +23,7 @@ function Header({ currentStep, completedSteps, onMaximizeClick }) {
                     <img src={MaximizeSvg} alt="Maximize" />
                 </button>
             </header>
-        </>
+        </Fragment>
     )
 }
 
